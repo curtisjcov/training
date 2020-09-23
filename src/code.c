6f6d945071adb38347d43e263081f046acc1f274
Curtis Covington <cjcov@byu.edu>
2020-09-23 16:18:07 -0600

    Source Code Works

--- src/code.c
+++ src/code.c
@@ -8,15 +8,27 @@ int main() {
 	double time_end = 1;
         double del_t = .000001;
 	
+	string l1;
+
+        ifstream input;
+        input.open("params.dat");
+        if (input.is_open())
+        {
+                while ( getline(input,l1))
+		{
+			stringstream(l1) >> del_t >> time_end;
+		}
+        }
+
 	double xi = 1;
 	double x = xi;
 	
-	ofstream output;
-	output.open ("output.dat");
-	 for (double i=0; i<=time_end; i+=del_t)
-        {
-                x= x*(1-3*del_t);
-		output << i << ' ' << x << '\n';
-        }
+		ofstream output;
+		output.open ("output.dat");
+		for (double i=0; i<time_end+del_t; i+=del_t)
+	      	{
+        	        x= x*(1-3*del_t);
+			output << i << ' ' << x << '\n';
+	        }
 	output.close();
 }	
