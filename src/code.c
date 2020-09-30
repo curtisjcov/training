be7c7f70754d114a57964559c03dbc68aaeba9d7
Curtis Covington <cjcov@byu.edu>
2020-09-30 12:21:45 -0600

    SRC_error

--- src/code.c
+++ src/code.c
@@ -4,31 +4,41 @@
 #include <string>
 using namespace std;
 
-int main() {
-	double time_end = 1;
-        double del_t = .000001;
+int main() 
+{
+	int trial_num = 7;
 	
-	string l1;
-
-        ifstream input;
-        input.open("params.dat");
-        if (input.is_open())
-        {
-                while ( getline(input,l1))
-		{
-			stringstream(l1) >> del_t >> time_end;
-		}
-        }
-
-	double xi = 1;
-	double x = xi;
-	
-		ofstream output;
-		output.open ("output.dat");
-		for (double i=0; i<time_end+del_t; i+=del_t)
-	      	{
-        	        x= x*(1-3*del_t);
-			output << i << ' ' << x << '\n';
+	for (int i = 0; i<trial_num; i++)
+	{
+		stringstream name_i;
+		name_i << "params_" << i << ".dat";
+	        ifstream input;
+	        input.open( name_i.str().cstr() );
+		string l;
+		double del_t;
+		double time_end;
+	        if (input.is_open())
+	        {
+	                while ( getline(input,l))
+			{
+				stringstream(l) >> del_t >> time_end;
+			}
 	        }
-	output.close();
+	
+		double xi = 1;
+		double x = xi;
+			
+		stringstream name_o;
+		name_o << "output_" <<  i << ".dat";	
+		str_o << name_o.str()
+		
+		ofstream output;
+		output.open ( name_o.str().cstr() );
+		for (double j=0; i<time_end+del_t; j+=del_t)
+     	 	{
+        	        x= x*(1-3*del_t);
+			output << j << ' ' << x << '\n';
+        	}
+		output.close();
+	}
 }	
