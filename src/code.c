70535a0637994215c2648551f57bfe705bde1374
Curtis Covington <cjcov@byu.edu>
2020-10-01 12:44:37 -0600

    code works

--- src/code.c
+++ src/code.c
@@ -8,12 +8,12 @@ int main()
 {
 	int trial_num = 7;
 	
-	for (int i = 0; i<trial_num; i++)
+	for (int i = 0; i<=trial_num; i++)
 	{
 		stringstream name_i;
 		name_i << "params_" << i << ".dat";
 	        ifstream input;
-	        input.open( name_i.str().cstr() );
+	        input.open( name_i.str().c_str() );
 		string l;
 		double del_t;
 		double time_end;
@@ -23,22 +23,22 @@ int main()
 			{
 				stringstream(l) >> del_t >> time_end;
 			}
-	        }
+	        
 	
-		double xi = 1;
-		double x = xi;
+			double xi = 1;
+			double x = xi;
 			
-		stringstream name_o;
-		name_o << "output_" <<  i << ".dat";	
-		str_o << name_o.str()
+			stringstream name_o;
+			name_o << "output_" <<  i << ".dat";	
 		
-		ofstream output;
-		output.open ( name_o.str().cstr() );
-		for (double j=0; i<time_end+del_t; j+=del_t)
-     	 	{
-        	        x= x*(1-3*del_t);
-			output << j << ' ' << x << '\n';
-        	}
-		output.close();
+			ofstream output;
+			output.open ( name_o.str().c_str() );
+			for (double j=0; j<=time_end+(del_t/10); j+=del_t)
+     	 		{
+        		        x= x*(1-3*del_t);
+				output << j << ' ' << x << '\n';
+        		}	
+			output.close();
+		}
 	}
 }	
